dae87f0d3716b8aaa9f94a45b33a7a060c5e3b51
Anni Voore <anni.voore@voco.ee>
2024-11-20 11:41:20 +0200

    Sündmused ja nende handler-id

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,26 @@
 import ExpenseDate from '../Expenses/ExpenseDate';
-import Card from "../UI/Card"
+import Card from "../UI/Card";
 import './ExpenseItem.css';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
-    const{date, title, price} = props;
-console.log(price)
+    const { date, title: initialTitle, price } = props;
+    const [title, setTitle] = useState(initialTitle);
+
+    const clickHandler = () => {
+        console.log('Clicked!');
+        setTitle(`Updated by click: ${title}`);
+        console.log(title);
+    };
+
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={date}/>
+            <ExpenseDate date={date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className="expense-item__price">{price}</div>
             </div>
+            <button onClick={clickHandler}>Click Me</button>
         </Card>
     );
 };
