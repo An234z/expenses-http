3bf861e855d30137184880b0be8858aad322ad51
Anni Voore <anni.voore@voco.ee>
2024-12-07 13:45:56 +0200

    Errori akna lisamine

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,72 +1,92 @@
-import { useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import "./ExpenseForm.css";
+import Error from "../UI/Error";
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredPrice, setEnteredPrice] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [error, setError] = useState(null); 
 
-  const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
-  };
+  const titleInputRef = useRef();
+  const priceInputRef = useRef();
+  const dateInputRef = useRef();
 
-  const priceChangeHandler = (event) => {
-    setEnteredPrice(event.target.value);
-  };
-
-  const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+  const errorHandler = () => {
+    console.log("Error modal closed!"); 
+    setError(null);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
+    const enteredTitle = titleInputRef.current.value;
+    const enteredPrice = priceInputRef.current.value;
+    const enteredDate = dateInputRef.current.value;
+
+   
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredPrice.trim().length === 0 ||
+      enteredDate.trim().length === 0
+    ) {
+      const errorData = {
+        title: "Invalid input",
+        message: "Please enter a valid title or price or date (non-empty values)",
+      };
+
+      console.log(errorData); 
+      setError(errorData);
+      return; 
+    }
+
     const expenseData = {
       title: enteredTitle,
-      price: enteredPrice,
+      price: parseFloat(enteredPrice),
       date: new Date(enteredDate),
     };
 
+    console.log(expenseData);
     props.onSaveExpenseData(expenseData);
+    props.onCancel();
 
-    setEnteredTitle("");
-    setEnteredPrice("");
-    setEnteredDate("");
+   
+    titleInputRef.current.value = "";
+    priceInputRef.current.value = "";
+    dateInputRef.current.value = "";
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-
-        <div className="new-expense__control">
-          <label>Price</label>
-          <input
-            type="number"
-            value={enteredPrice}
-            onChange={priceChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input type="date" value={enteredDate} onChange={dateChangeHandler} />
-        </div>
-        </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-          <button type="button" onClick={props.onCancel}>
-            Cancel
-          </button>
-        </div>
-      
-    </form>
+    <Fragment>
+      {error && (
+        <Error
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <div>
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input type="text" ref={titleInputRef} />
+            </div>
+            <div className="new-expense__control">
+              <label>Price</label>
+              <input type="number" ref={priceInputRef} />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input type="date" ref={dateInputRef} />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="submit">Add Expense</button>
+            <button type="button" onClick={props.onCancel}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </Fragment>
   );
 };
 
